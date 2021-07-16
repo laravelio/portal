bca60cd94df36b4211ee4796f8738d000ec045db
Joe Dixon <hello@joedixon.co.uk>
2021-07-16 13:31:04 +0100

    Update profile block

--- resources/views/components/moderators.blade.php
+++ resources/views/components/moderators.blade.php
@@ -1,6 +1,6 @@
 @props(['moderators'])
 
-<div class="bg-white shadow rounded-md mt-6">
+<div class="bg-white shadow rounded-md">
     <h3 class="text-xl font-semibold px-5 pt-5">Moderators</h3>
     <ul>
         @foreach ($moderators as $moderator)
@@ -8,7 +8,7 @@
                 <a href="{{ route('profile', $moderator->username()) }}" class="hover:underline">
                     <x-avatar :user="$moderator" class="w-10 h-10" />
                 </a>
-                
+
                 <span class="flex flex-col">
                     <a href="{{ route('profile', $moderator->username()) }}" class="hover:underline">
                         <span class="text-gray-900 font-medium">
