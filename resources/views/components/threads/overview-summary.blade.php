2f344a763395f93f6ab8de1342da55adecf4703c
Dries Vints <dries.vints@gmail.com>
2021-07-18 15:05:51 +0200

    Update overview-summary.blade.php

--- resources/views/components/threads/overview-summary.blade.php
+++ resources/views/components/threads/overview-summary.blade.php
@@ -8,8 +8,7 @@
             <div class="flex flex-col lg:flex-row lg:items-center">
                 <div>
                     <a href="{{ route('profile', $thread->author()->username()) }}" class="flex items-center hover:underline">
-
-                    <x-avatar :user="$thread->author()" class="w-6 h-6 rounded-full mr-3" />
+                        <x-avatar :user="$thread->author()" class="w-6 h-6 rounded-full mr-3" />
                         <span class="text-gray-900 mr-5">{{ $thread->author()->name() }}</span>
                     </a>
                 </div>
